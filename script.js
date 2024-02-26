230e3306cd9218dbc3d10931608e63084dce5008
yskim308 <youngseokim7@gmail.com>
2024-02-26 17:30:09 +0900

    add initial book creation logic and DOM manipulation
    add js logic for DOM manipulation where every book in myLibrary is printed onto a div

--- script.js
+++ script.js
@@ -17,4 +17,45 @@ const animalFarm = new Book("Animal Farm", "George Orwell", 200);
 const book2 = new Book("generic book", "Jesus", 300); 
 addBooktoLibrary(animalFarm);
 addBooktoLibrary(book2); 
-console.log(myLibrary);
+
+myLibrary.forEach((book) =>{
+    console.log(book.title);
+    console.log(book.author);
+    console.log(book.pages);
+})
+
+myLibrary.forEach((book)=>{
+    const container = document.querySelector(".content");
+    const card = document.createElement("div")
+    card.classList.add("card");
+
+    //set text content for new elements inside 'card'
+    const title = document.createElement("p")
+    title.classList.add("title");
+    title.textContent = book.title; 
+
+    const author = document.createElement("p")
+    author.classList.add("author");
+    author.textContent = book.author; 
+
+    const pages = document.createElement("p")
+    pages.classList.add("pages"); 
+    pages.textContent = book.pages; 
+    
+    // append the title, author, and pages to the card div 
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages); 
+    
+    //add and append the two buttons to the card div 
+    const read = document.createElement("button")
+    read.classList.add("read"); 
+    card.appendChild(read); 
+    
+    const remove = document.createElement("button")
+    remove.classList.add("remove"); 
+    card.appendChild(remove); 
+
+    //now add the newly created card to the original container
+    container.appendChild(card); 
+})
